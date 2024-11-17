22d0930af33dbad19eaa69e1d63edf2975316b49
Bhargavi Prasad <bhargavi.prasad@rhibhus.com>
2024-11-17 22:42:54 +0530

    vsrepx -> cs

--- AspNetCore.Reporting.Angular/Reports/CourseListReport.cs
+++ AspNetCore.Reporting.Angular/Reports/CourseListReport.cs
@@ -1,6 +1,15 @@
-namespace AspNetCore.Reporting.Common.Reports {
-    public partial class CourseListReport {
-        public CourseListReport() {
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace AspNetCore.Reporting.Common.Reports
+{
+    public partial class CourseListReport : DevExpress.XtraReports.UI.XtraReport
+    {
+        public CourseListReport()
+        {
             InitializeComponent();
         }
     }
--- AspNetCore.Reporting.Angular/Reports/MyEnrollmentsReport.cs
+++ AspNetCore.Reporting.Angular/Reports/MyEnrollmentsReport.cs
@@ -1,6 +1,15 @@
-namespace AspNetCore.Reporting.Common.Reports {
-    public partial class MyEnrollmentsReport {
-        public MyEnrollmentsReport() {
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace AspNetCore.Reporting.Common.Reports
+{
+    public partial class MyEnrollmentsReport : DevExpress.XtraReports.UI.XtraReport
+    {
+        public MyEnrollmentsReport()
+        {
             InitializeComponent();
         }
     }
